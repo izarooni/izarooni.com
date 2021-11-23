2347ecf9854f461774b698afca5bfc8797211e8e
Ian <coalasonly@gmail.com>
2021-11-22 18:21:21 -0700

    update socials

--- pages/index.tsx
+++ pages/index.tsx
@@ -79,7 +79,7 @@ export default function Home(props: any) {
                             <nav className="level is-mobile is-size-3 is-justify-content-space-evenly">
                                 <a href="https://github.com/izarooni"><i className="level-item fab fa-github-alt"></i></a>
                                 <a href="https://www.linkedin.com/in/ian-conyers"><i className="level-item fab fa-linkedin-in"></i></a>
-                                <a href="https://www.instagram.com/izarooni/"><i className="level-item fab fa-instagram"></i></a>
+                                <a href="https://www.instagram.com/play.izarooni/"><i className="level-item fab fa-instagram"></i></a>
                             </nav>
                         </div>
                     </div>
